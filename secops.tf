464dd265ada4885f599ef7a995f6748942f16367
Anil Choubey <anilchoubey@Anils-MacBook-Air.local>
2026-02-16 01:12:45 +0000

    test app

---
+++ secops.tf
@@ -0,0 +1,7 @@
+module "project_name_oidc" {
+  source    = "app.terraform.io/AnchoAutomatedSolutions/okta-App/okta"
+  version   = "1.0.0"
+  app_type  = "oidc"
+  app_label = "Project-X-Frontend"
+  redirect_uris = ["[https://x.company.com/callback](https://x.company.com/callback)"]
+}
