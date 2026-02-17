409ee73e5dc8fee4cc31b2b52a3cbd825796606f
Anil Choubey <anil_choubey@hotmail.com>
2026-02-17 16:04:07 +0000

    new saml app

---
+++ testteam.tf
@@ -0,0 +1,25 @@
+
+data "okta_group" "existing_app_group" {
+  name = "Everyone"
+}
+
+module "portal_saml_app" {
+  source    = "app.terraform.io/AnchoAutomatedSolutions/okta-App/okta"
+  version   = "1.0.8" 
+
+  app_type  = "saml"
+  app_label = "Test1-App-SAML"
+
+  
+  sso_url   = "https://portal1.company.com/saml/acs"
+  audience  = "https://portal.company.com/metadata"
+
+
+  group_ids = [data.okta_group.existing_app_group.id]
+
+}
+
+
+output "portal_metadata_url" {
+  value = module.portal_saml_app.saml_metadata_url
+}
